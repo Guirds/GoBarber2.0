0f2ad17584fe05211a54f9e5f4542e46100cbb9c
Guilherm Santos <gui07015@gmail.com>
2019-09-29 16:07:57 -0300

    feat: Criando migration de usuário

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # GoBarber2.0
 
-Dependencias:
-Database:
+#Database:
+##Dependencias:
 1- sequelize
 2- sequelize-cli -D (Fácilita na execuçãode migrations,criação de models...)
-3- pg pg-hstore( Dependencias do BD postgres)
+3- pg pg-hstore (Dependencias do BD postgres)
+
+##Comandos:
+Com BD de dados rodando (Docker) digitar executar os comandos:
+yarn sequelize migration:create --name=create-users (Cria a migration, com nome users)
+yanr sequelize db:migrate (Cria a tabela no banco de dados)
+
+###Se for necessário fazer alguma alteração nas migrations:
+yarn sequelize db:migrate:undo (Desfaz a última migration)
+yarn sequelize db:migrate:undo:all(Desfaz todas as migrations)
+Obs: É uma boa prática desfazer as migrations, alterar elas e depois executar ela novamente.
---
+++ src/database/migrations/20190929184337-create-users.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // não pode ser null
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, // Sem e-mails repetidos
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
+      // O preenchimento desses campos será feito automaticamente pelo sequelize
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      // O preenchimento desses campos será feito automaticamente pelo sequelize
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
