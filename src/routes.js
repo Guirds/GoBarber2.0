e76592f6a7fd50eee06dc48900113c9c2c9caa0c
Guilherm Santos <gui07015@gmail.com>
2019-09-29 17:01:59 -0300

    Testando conexão ao BD com uma requisição direta da rota

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 
+// Testando conexão com bando de dados
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {});
+// É necessário utilizar o async await, devido a "demora" de resposta ao executar a opereção no BD
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme',
+    email: 'gui07015@gmail.com',
+    password_hash: '15646113',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
